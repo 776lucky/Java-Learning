fbdc3893978433bd1ed215b90afeb6f8e3c90358
776lucky <zhuangwenyuan7@gmail.com>
2024-10-02 12:03:23 +1000

    java enumerate example

---
+++ Java_enumerate.java
@@ -0,0 +1,10 @@
+enum color {
+    RED, BLUE, GREEN;
+}
+
+public class Java_enumerate {
+    public static void main(String[] args) {
+        color c1 = color.BLUE;
+        System.out.println(c1);
+    }
+}
